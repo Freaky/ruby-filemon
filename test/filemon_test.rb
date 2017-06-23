ce9c67faf26826a6e7e13b9c74a0b50b04aaf9f6
Thomas Hurst <tom@hur.st>
2017-06-23 22:29:51 +0100

    Add some basic tests.

--- test/filemon_test.rb
+++ test/filemon_test.rb
@@ -5,7 +5,22 @@ class FilemonTest < Minitest::Test
     refute_nil ::Filemon::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_device_lifecycle
+    fm = Filemon::Device.new
+    refute fm.closed?
+    fm.close
+    assert fm.closed?
+  end
+
+  def test_self_trace
+    r,w = IO.pipe
+
+    fm = Filemon::Device.new(fd: w, pid: $$)
+    fm.close
+    w.close
+    ret = r.readlines
+
+    assert_match(/\A# filemon version 5/, ret.shift)
+    assert_match(/\A# Target pid #{$$}/, ret.shift)
   end
 end
