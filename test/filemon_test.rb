fc5e9f648c1054ea029196073050d16c9454d602
Thomas Hurst <tom@hur.st>
2017-06-24 22:32:15 +0100

    Tests: Sketch out a possible higher-level interface

--- test/filemon_test.rb
+++ test/filemon_test.rb
@@ -23,4 +23,23 @@ class FilemonTest < Minitest::Test
     assert_match(/\A# filemon version 5/, ret.shift)
     assert_match(/\A# Target pid #{$$}/, ret.shift)
   end
+
+  def test_trace
+    skip # proposed interface
+
+    trace = Filemon::Trace.new do
+      this_file = File.new(__FILE__)
+      devnull = File.new('/dev/null', 'w')
+      IO.copy_stream(this_file, devnull)
+    end
+
+    assert trace.events.one? do |event|
+      event.type == :read && event.path == __FILE__
+    end
+
+    assert trace.events.one? do |event|
+      event.type == :write && event.path = '/dev/null'
+    end
+  end
 end
+
